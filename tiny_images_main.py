f3d7901c30b3eb705520641eb58bee4d2456631d
Eric Chheang <et.chheang@gmail.com>
2019-12-04 17:58:22 -0500

    formatted original dataset to fit TF models input

--- tiny_images_main.py
+++ tiny_images_main.py
@@ -1,23 +1,46 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import tensorflow as tf
+from skimage.transform import resize
+
+keras = tf.keras 
 
 
-base_dir = '/tiny_image_data/ukycs460g2019p1/'
+base_dir = './tiny_image_data/ukycs460g2019p1/'
+IMG_SIZE = 160 # All images will be resized to 160x160
+im_train = [] 
+im_test = [] 
 
 # load features, ignore header, ignore IDs
 X_train = np.loadtxt(base_dir + 'train_features.csv', delimiter=',')[:,1:]
 X_test = np.loadtxt(base_dir + 'test_features.csv', delimiter=',')[:,1:]
 y_train = np.loadtxt(base_dir + 'train_labels.csv', dtype=np.uint8, delimiter=',', skiprows=1)[:,-1]
+#print(X_train.shape)
+
+# resizing our image data
+for img in X_train:
+    trainImg = img.reshape((30,30,3), order='F')
+    testImg = img.reshape((30,30,3), order='F')
+    train_res = resize(trainImg, (IMG_SIZE,IMG_SIZE,3))
+    test_res = resize(testImg, (IMG_SIZE,IMG_SIZE,3))
+    im_train.append(train_res)
+    im_test.append(test_res)
+
+# convert to np arrays
+train_X = np.array(im_train)
+test_X = np.array(im_test)
+
+# our training data should have format (n, height, width, channel) for our desired TF model
+# print(train_X.shape)
+# print(y_train.shape)
+# print(test_X.shape)
 
 
-im_train = X_train[0,:].reshape((30,30,3), order='F')
-im_test = X_test[0,:].reshape((30,30,3), order='F')
+# plt.figure(1)
+# plt.imshow(im_train/255)
+# plt.axis('off')
 
-plt.figure(1)
-plt.imshow(im_train/255)
-plt.axis('off')
 
-plt.figure(2)
-plt.imshow(im_test/255)
-plt.axis('off')
+# have images show in mac terminal
+#plt.show()
