2d1e8896d9219dfe143483f44cfe9d0d18a66208
EricChh20 <42673389+EricChh20@users.noreply.github.com>
2019-12-07 22:29:59 -0500

    passed training and prediction

--- tiny_images/main.py
+++ tiny_images/main.py
@@ -3,28 +3,80 @@ import numpy as np
 import os
 import keras
 import matplotlib.pyplot as plt
+import tensorflow as tf 
+
+from keras.layers import Dense,GlobalAveragePooling2D,Flatten
+from keras.applications import MobileNet
+from keras.preprocessing import image
+from keras.applications.mobilenet import preprocess_input
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Model
+from keras.optimizers import Adam
+from skimage.transform import resize
+
 
 base_dir = './tiny_image_data/ukycs460g2019p1/'
-IMG_SIZE = 75 # All images will be resized to 160x160
+IMG_SIZE = 160 # All images will be resized to 160x160
 im_test = [] 
+im_train = [] 
+sol = []
 
 # load features, ignore header, ignore IDs
+X_train = np.loadtxt(base_dir + 'train_features.csv', delimiter=',')[:,1:]
 X_test = np.loadtxt(base_dir + 'test_features.csv', delimiter=',')[:,1:]
+y_train = np.loadtxt(base_dir + 'train_labels.csv', dtype=np.uint8, delimiter=',', skiprows=1)[:,-1]
 #print(X_train.shape)
 
 # resizing our image data
 for img in X_train:
-    testImg = img.reshape((30,30,3), order='F')
+    trainImg = img.reshape((30,30,3))
+    train_res = resize(trainImg, (IMG_SIZE,IMG_SIZE,3))
+    im_train.append(train_res)
+    
+for img in X_test:
+    testImg = img.reshape((30,30,3))
     test_res = resize(testImg, (IMG_SIZE,IMG_SIZE,3))
-    im_test.append(test_res)
+    im_test.append(test_res)    
+
 # convert to np arrays
+train_X = np.array(im_train)
 test_X = np.array(im_test)
 
-model.load_weights("model2.h5")
+print(len(train_X))
+print(train_X[1].shape)
+print(len(test_X))
+print(test_X[1].shape)
+
+# base_model=MobileNet(weights='imagenet',include_top=False) #imports the mobilenet model and discards the last 1000 neuron layer.
+base_model = MobileNet(include_top=False)
+x=base_model.output
+x=GlobalAveragePooling2D()(x)
+x=Dense(1024,activation='relu')(x) #we add dense layers so that the model can learn more complex functions and classify for better results.
+x=Dense(512,activation='relu')(x) #dense layer 3
+preds=Dense(28,activation='softmax')(x) #final layer with softmax activation
+
+model=Model(inputs=base_model.input,outputs=preds)
+for layer in model.layers:
+    layer.trainable=False
+# or if we want to set the first 20 layers of the network to be non-trainable
+for layer in model.layers[:20]:
+    layer.trainable=False
+for layer in model.layers[20:]:
+    layer.trainable=True
+
+model.compile(optimizer='Adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+model.fit(train_X, y_train, epochs=10)
+
+model.save_weights("mobilenet_tiny_img2.h5")
 
 pred = model.predict(test_X)
-max_value = max(pred[6])
-max_index = np.where(pred[6]==max_value)
+print(pred[5])
 
-print(max_value)
-print(max_index)
+for i in range(0,len(pred)):
+    max_value = max(pred[i])
+    max_index = np.where(pred[i]==max_value)
+    sol.append(max_index)
+print(sol)
+df = pd.DataFrame(sol, columns=['Label'])
+df.index += 1 # "upgrade" to one-based indexing
+df.to_csv('knn_submission.csv',index_label='ID',columns=['Label'])
